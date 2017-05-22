7f63f3d3defc3347800d31eb83425caf88e01817
d-jeph <jephdavid@gmail.com>
2017-05-23 01:57:56 +0300

    add seettings and requirements file

---
+++ jokes/config/settings/prd.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
